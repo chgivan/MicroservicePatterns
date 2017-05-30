3ffa386e777adc17c3a98dd82f45f229516879ad
Chris Givanoudis <chgivan@gmail.com>
2017-05-30 16:05:10 +0300

    Remove bug files for fixing

--- pattern_five/client.py
+++
@@ -1,8 +0,0 @@
-import pika, json, sys
-
-host = "192.168.99.100"
-
-connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
-channel = connection.channel()
-result = channel.queue_declare(exclusive=True)
-callback_queue = result.method.queue
